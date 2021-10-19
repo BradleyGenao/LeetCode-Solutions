77f08b0c661ae7c47c20c2781415614a07bdba7e
Bradley Genao <35042442+BradleyGenao@users.noreply.github.com>
2021-10-19 16:50:44 -0700

    Time: 44 ms (82.24%), Space: 14.6 MB (56.15%) - LeetHub

--- valid-anagram/valid-anagram.py
+++ valid-anagram/valid-anagram.py
@@ -1,28 +1,19 @@
+import collections
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
+        return self.build_dict(s) == self.build_dict(t)
         
-        if len(s) != len(t):
-            return False
-        
-        s_dic = self.get_string_count(s)
-        t_dic = self.get_string_count(t)
-        
-        return s_dic == t_dic
         
     
     
-    
-    
-    
-    
-    def get_string_count(self, string):
-        string_dic = {}
+    def build_dict(self, string):
+        ana_dict = defaultdict(int)
+        
+        
         for char in string:
-            if char not in string_dic:
-                string_dic[char] = 1
-            else:
-                string_dic[char] += 1
-        return string_dic
-                
+            ana_dict[char] +=1
+        return ana_dict
+        
         
