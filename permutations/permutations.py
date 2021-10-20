e798d39286d4a7d0f28ff715cd0296078be4e85c
Bradley Genao <35042442+BradleyGenao@users.noreply.github.com>
2021-10-20 12:14:07 -0700

    Time: 73 ms (9.08%), Space: 14.5 MB (43.01%) - LeetHub

--- permutations/permutations.py
+++ permutations/permutations.py
@@ -2,18 +2,18 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         
         
-        result = []
-        self.find_perms(nums, 0, [], result)
-        return result
+        answer = []
+        self.permute_rec(nums, 0, [], answer)
+        return answer
     
-    def find_perms(self,nums, index, curr_perm, result ):
+    def permute_rec(self, nums, index, curr_perm, answer):
         if len(nums) == index:
-            result.append(curr_perm)
+            answer.append(curr_perm)
         else:
             for i in range(len(curr_perm) + 1):
                 new_perm = list(curr_perm)
                 new_perm.insert(i, nums[index])
-                self.find_perms(nums, index+1, new_perm, result)
+                self.permute_rec(nums, index+1, new_perm, answer)
         
-                
+        
         
