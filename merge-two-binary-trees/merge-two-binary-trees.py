4016dc1012c9fcedc129cf3da45af35c921d21e9
Bradley Genao <35042442+BradleyGenao@users.noreply.github.com>
2021-11-08 12:27:18 -0800

    Time: 84 ms (83.24%), Space: 15.8 MB (14.93%) - LeetHub

--- merge-two-binary-trees/merge-two-binary-trees.py
+++ merge-two-binary-trees/merge-two-binary-trees.py
@@ -6,27 +6,22 @@
 #         self.right = right
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root1: return root2
+        if not root2: return root1
+        queue1 = [(root1)]
+        queue2 = [(root2)]
         
-        queue1 = [root1]
-        queue2 = [root2]
-        
-        if not (root1 and root2):
-            return root1 or root2
         while queue1 and queue2:
             node1, node2 = queue1.pop(0), queue2.pop(0)
             
             if node1 and node2:
-                node1.val = node1.val + node2.val
-                if (not node1.left) and node2.left:
+                node1.val += node2.val
+                if not node1.left and node2.left:
                     node1.left = TreeNode(0)
-                if (not node1.right) and node2.right:
+                if not node1.right and node2.right:
                     node1.right = TreeNode(0)
                 queue1.append(node1.left)
                 queue1.append(node1.right)
                 queue2.append(node2.left)
                 queue2.append(node2.right)
-        
-        
         return root1
-        
-        
