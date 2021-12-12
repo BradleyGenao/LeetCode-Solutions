d9522d9b0cd2972f453e5c50f8408fbc173e1359
Bradley Genao <35042442+BradleyGenao@users.noreply.github.com>
2021-12-12 09:33:13 -0800

    Time: 132 ms (70.46%), Space: 16.5 MB (96.74%) - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -1,22 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         
+        counter = collections.Counter(nums)
         
+        for number, count in counter.items():
+            if count == 1:
+                return number
         
-        
-        
-        
-        if len(nums) == 1:
-            return nums[0]
-        
-        half = len(nums) //2 
-        left = nums[:half]
-        right = nums[half:]
-        
-        for num in left:
-            if nums.count(num) == 1:
-                return num
-        
-        for num in right:
-            if nums.count(num) == 1:
-                return num
