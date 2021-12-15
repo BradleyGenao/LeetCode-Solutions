d7dc8fde590e661eb1d8d59d5ebadf7bfb0bc1a5
Bradley Genao <35042442+BradleyGenao@users.noreply.github.com>
2021-12-15 10:55:04 -0800

    Time: 536 ms (71.97%), Space: 53 MB (37.18%) - LeetHub

--- minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -6,36 +6,17 @@
 #         self.right = right
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
+        
         if not root:
             return 0
         
-        """
-        Solved by using a queue and a dictionary
         
-        key = depth
-        value = nodes at that level
+        if not root.left:
+            return self.minDepth(root.right) + 1
+        if not root.right:
+            return self.minDepth(root.left) + 1
         
-        At the end i just iterate through the list looking 
-        for first leaf
-        """
-        queue, depth_map = collections.deque([root]), collections.defaultdict(list)
-        res = []
-        depth = 0
+        return min(self.minDepth(root.left), self.minDepth(root.right) ) + 1
         
-        while queue:
-            lvl = []
-            depth +=1
-            for _ in range(len(queue)):
-                curr = queue.popleft()
-                depth_map[depth].append(curr)
-                lvl.append(curr.val)
-                if curr.left:
-                    queue.append(curr.left)
-                if curr.right:
-                    queue.append(curr.right)
-            res.append(lvl)
-        for key, value in depth_map.items():
-            for node in value:
-                if not node.left and not node.right:
-                    return key
-                
+        
+        
