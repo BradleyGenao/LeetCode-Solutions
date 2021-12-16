93b8306f2a2654d92b59f56a58b787e9c6513d3c
Bradley Genao <35042442+BradleyGenao@users.noreply.github.com>
2021-12-16 10:49:29 -0800

    Time: 103 ms (17.27%), Space: 17.5 MB (10.20%) - LeetHub

--- meeting-rooms/meeting-rooms.py
+++ meeting-rooms/meeting-rooms.py
@@ -3,8 +3,7 @@ class Solution:
         
         intervals.sort()
         
-        for i in range(len(intervals) - 1):
+        for i in range(len(intervals) -1):
             if intervals[i][1] > intervals[i+1][0]:
                 return False
         return True
-        
