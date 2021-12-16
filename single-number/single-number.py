86cc1e481cca14af1f699d6d193c54b7b6dead29
Bradley Genao <35042442+BradleyGenao@users.noreply.github.com>
2021-12-16 10:07:02 -0800

    Time: 177 ms (27.64%), Space: 16.6 MB (85.91%) - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -1,9 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         
-        counter = collections.Counter(nums)
+        xor_check = 0
         
-        for number, count in counter.items():
-            if count == 1:
-                return number
+        for num in nums:
+            xor_check ^= num
+        return xor_check
         
