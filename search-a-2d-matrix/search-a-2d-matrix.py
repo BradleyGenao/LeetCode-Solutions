4c727757a8687743520cdd60cf7937439edba596
Bradley Genao <35042442+BradleyGenao@users.noreply.github.com>
2021-12-17 11:02:21 -0800

    Time: 80 ms (5.28%), Space: 14.7 MB (64.61%) - LeetHub

--- search-a-2d-matrix/search-a-2d-matrix.py
+++ search-a-2d-matrix/search-a-2d-matrix.py
@@ -4,15 +4,14 @@ class Solution:
         
         m, n = len(matrix), len(matrix[0])
         
-        if m ==1 and n==1:
-            return matrix[0][0] == target
-        left, right = 0, m*n - 1
+        left, right = 0, m*n -1
         
         while left < right:
-            mid = left + (right - left) //2
-            if matrix[mid // n][mid % n] >= target:
+            mid = left + (right - left) // 2
+            
+            if matrix[mid//n][mid%n] >= target:
                 right = mid
             else:
                 left = mid + 1
-        return matrix[left//n][left %n] == target
+        return matrix[left//n][left%n] == target
         
