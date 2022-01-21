902d847c8affb73c2a5f98654c38e5248cd2802a
Bradley Genao <35042442+BradleyGenao@users.noreply.github.com>
2022-01-20 20:19:40 -0800

    Time: 136 ms (36.92%), Space: 15.4 MB (85.94%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,28 +1,26 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         
-        
-        
-        
         graph = defaultdict(set)
         in_degrees = defaultdict(int)
-        for i in range(numCourses):
-            graph[i].add(i)
-            in_degrees[i] =0
-        for nxt_course, prev_course in prerequisites:
-            graph[nxt_course].add(prev_course)
-            in_degrees[prev_course] +=1
+        
+        {graph[preq].add(nxt) for preq, nxt in prerequisites }
+        {in_degrees[i] for i in range(numCourses) }
+        for _, prev in prerequisites:
+            in_degrees[prev] +=1
         queue, seen = deque(), set()
+        
         for index, in_degree in in_degrees.items():
             if in_degree == 0:
                 queue.append(index)
-        while queue:
-            index = queue.popleft()
-            seen.add(index)
-            for g in graph[index]:
-                in_degrees[g] -=1
-                if in_degrees[g] == 0:
-                    queue.append(g)
-        return len(seen) == numCourses
         
+        while queue:
+            curr = queue.popleft()
+            seen.add(curr)
+            for neigh in graph[curr]:
+                in_degrees[neigh] -=1
+                if in_degrees[neigh] == 0:
+                    queue.append(neigh)
+        return len(seen) == numCourses
+
         
