595abfc1bde405d7bd010a6bf7b30372a49f75c8
Bradley Genao <35042442+BradleyGenao@users.noreply.github.com>
2022-01-20 19:24:39 -0800

    Time: 464 ms (69.19%), Space: 30.3 MB (41.64%) - LeetHub

--- 148-sort-list/148-sort-list.py
+++ 148-sort-list/148-sort-list.py
@@ -7,16 +7,15 @@ class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
             return head
-        
         def get_mid(turtle):
             hare = turtle.next
             while hare and hare.next:
                 turtle = turtle.next
                 hare = hare.next.next
-            return turtle    
-             
+            return turtle
+
         def merge(left, right):
-            dummy = ans = ListNode(-1)
+            dummy = merged = ListNode(-1)
             while left and right:
                 if left.val < right.val:
                     dummy.next = left
@@ -29,19 +28,18 @@ class Solution:
                 dummy.next = left
             if right:
                 dummy.next = right
-            return ans.next
+            return merged.next
 
-
-       
         left = head
         right = get_mid(head)
         temp = right.next
         right.next = None
-        right = temp        
-
-        left = self.sortList(left)
-        right = self.sortList(right) 
+        right = temp
         
+   
+        left = self.sortList(left)
+        right = self.sortList(right)
+   
         return merge(left, right)
 
         
