3b4e9433990f550ee2af1afd9711920aa53dd43e
Bradley Genao <35042442+BradleyGenao@users.noreply.github.com>
2022-02-01 12:53:32 -0800

    Time: 235 ms, Memory: 15.7 MB - LeetHub

--- move-zeroes/move-zeroes.py
+++ move-zeroes/move-zeroes.py
@@ -3,12 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        pos = 0
         
+        
+        pos = 0
         for i, num in enumerate(nums):
-            if num !=0:
-                if pos != i:
+            if num != 0:
+                if i != pos:
                     nums[i], nums[pos] = nums[pos], nums[i]
                 pos +=1
-        return nums
-            
+        return 
